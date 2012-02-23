f29991b4847e152a7acdda5b5a36f11db4b4096f
joe <joe@Rose>
2012-02-22 21:18:43 -0500

    Implemented a basic logout script that I developed for the in-class sessions project.  This should work with just the session_destroy() method, but we need to implement the rest of the PHP scripts to test this.

--- gamers.sql
+++ gamers.sql
@@ -10,10 +10,10 @@ USE Luchalink;
 DROP TABLE IF EXISTS `Users`;
 CREATE TABLE `Users`(
 	`player_id` int(100) NOT NULL AUTO_INCREMENT,
-	`first_name` varchar(20) NOT NULL DEFAULT '',
-	`last_name` varchar(30) NOT NULL DEFAULT '',
+	`first_name` varchar(20) DEFAULT 'n00b',
+	`last_name` varchar(30) DEFAULT 'n00b',
 	`email` varchar(100) NOT NULL DEFAULT '',
-	`user_name` varchar(20) NOT NULL,
+	`user_name` varchar(20) DEFAULT 'n00b',
 	PRIMARY KEY (`player_id`)
 	-- NEED FOREIGN KEY TO JOIN TABLES...USE `user_name` as F.K.
 );
@@ -24,13 +24,13 @@ CREATE TABLE `Users`(
 
 DROP TABLE IF EXISTS `Players`;
 CREATE TABLE `Players`(
-	`user_name` varchar(20) NOT NULL,
+	`user_name` varchar(20) DEFAULT 'n00b',
 	-- password will use SHA1 hash, that's 40 characters long
 	`password` varchar(40) NOT NULL,
-	`wins` int NOT NULL,
-	`losses` int NOT NULL,
-	`ties`  int NOT NULL,
+	`wins` int DEFAULT 0,
+	`losses` int DEFAULT 0,
+	`ties`  int DEFAULT 0,
 	-- (5,2) = aaa.bb format
-	`win/loss_ratio` dec(5,2) NOT NULL,
+	`win/loss_ratio` dec(5,2) DEFAULT 000.00,
 	PRIMARY KEY (`user_name`)
 );
