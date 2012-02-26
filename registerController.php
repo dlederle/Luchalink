08db9c722663febfa9eda1f140b354f1071941fb
Dylan Lederle-Ensign <dylan.lederle.ensign@gmail.com>
2012-02-26 14:46:25 -0500

    register works, doesnt check uniques

--- registerController.php
+++ registerController.php
@@ -1,17 +1,20 @@
 <?php
      include "db_connect.php";
 
-     $username = $_POST['username'];
+     $email = $_POST['email'];
      $pw = $_POST['password'];
      //We're storing in plaintext for sprint 1, we can work on the encryption for sprint 2
-     $testquery = "SELECT * FROM users WHERE username = $username";
-     if(!is_null($testquery)):
-          header(/index.html?error=notunique);
+     //$testquery = "SELECT * FROM Users WHERE email = '$email'";
+     //$testresult = mysqli_query($db, $testquery);
+     //if(!is_null($testresult)):
+          //echo($testquery);
+          //echo($testresult);
+          //header("Location:   index.html?error=notunique");
           //Check, if not null, user exists, return an error message
-     else:
+     //else:
           //Else, create the user and log them in
-          $query = "INSERT INTO users (username, password) VALUES ('$username', '$pw')";
+          $query = "INSERT INTO Users (email, password) VALUES ('$email', '$pw')";
           $result = mysqli_query($db, $query);
-          //header(/loginController);
-     endif;
+          header("Location:   index.html");
+     //endif;
 ?>
