2506f5275546c8f5d052b33dfee67ac5761de599
Dylan Lederle-Ensign <dylan.lederle.ensign@gmail.com>
2012-02-26 13:39:52 -0500

    register stubbed out

--- registerController.php
+++ registerController.php
@@ -4,8 +4,13 @@
      $username = $_POST['username'];
      $pw = sha1($_POST['pw']);
      $testquery = "SELECT * FROM users WHERE username = $username";
-     //Check, if not null, return an error message
-
-     $query = "INSERT INTO users (username, password) VALUES ('$username', '$pw')";
-     $result = mysqli_query($db, $query);
+     if(!is_null($testquery)):
+          header(/index.html?error=notunique);
+          //Check, if not null, user exists, return an error message
+     else:
+          //Else, create the user and log them in
+          $query = "INSERT INTO users (username, password) VALUES ('$username', '$pw')";
+          $result = mysqli_query($db, $query);
+          header(/dashboard.
+     endif;
 ?>
