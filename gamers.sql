aa2602351e977a3ad27735e65fb8998ca1069fc3
kproffitt <kproffit@mail.umw.edu>
2012-03-15 08:43:10 -0400

    Schema change to the DB.  Should now be in 2NF.  Add comments to this particular commit if the DB is in 3NF, or edit commit message.

--- gamers.sql
+++ gamers.sql
@@ -17,20 +17,29 @@ CREATE TABLE `Users`(
 	`player_id` int(100) NOT NULL AUTO_INCREMENT,
 	`first_name` varchar(20) DEFAULT 'n00b',
 	`last_name` varchar(30) DEFAULT 'n00b',
+	 PRIMARY KEY (`player_id`)
+);
+
+DROP TABLE IF EXISTS `User_info`;
+CREATE TABLE `Users_info`(
+	`player_id` int NOT NULL AUTO_INCREMENT,
 	`email` varchar(100) NOT NULL DEFAULT '',
 	`password` varchar(40) NOT NULL,
-	-- password will use SHA1 hash, that's 40 characters long
-	`user_name` varchar(20) DEFAULT 'n00b',
-	PRIMARY KEY (`player_id`)
-	-- NEED FOREIGN KEY TO JOIN TABLES...USE `user_name` as F.K.
+	`display_name` varchar(20) DEFAULT 'n00b'
+	`profile_pic` blob,
+	CONSTRAINT users_player_id_fk
+	FOREIGN KEY (player_id)
+	REFERENCES Users (player_id)
 );
 
 --
--- Table structure for table `player`
+-- Table structure for table `Avatars`
 --
 
 DROP TABLE IF EXISTS `Avatars`;
 CREATE TABLE `Avatars`(
+	`game_id` int NOT NULL,
+	`avatar_id` int NOT NULL,
 	`user_name` varchar(20) DEFAULT 'n00b',
 	`rank` varchar(30) DEFAULT 'n00b',
 	`wins` int DEFAULT 0,
@@ -38,11 +47,14 @@ CREATE TABLE `Avatars`(
 	`ties`  int DEFAULT 0,
 	-- (5,2) = aaa.bb format
 	`win/loss_ratio` dec(5,2) DEFAULT 000.00,
-	PRIMARY KEY (`user_name`)
+	PRIMARY KEY (avatar_id),
+	CONSTRAINT users_avatar_id_fk
+	FOREIGN KEY (avatar_id)
+	REFERENCES Users (player_id)
 );
 
 --
--- Table structure for table `games`
+-- Table structure for table `Games`
 --
 
 DROP TABLE IF EXISTS `Games`;
@@ -54,6 +66,18 @@ CREATE TABLE `Games`(
 	`rank` int DEFAULT 1500,
 	PRIMARY KEY(`title`)
 );
+
+--
+-- Table structure for table `Friends`
+--
+
+DROP TABLE IF EXISTS `Friends`;
+CREATE TABLE `Friends`(
+	`player_id` int NOT NULL,
+	`friend_id` int NOT NULL,
+);
+
+-- POPULATE THE FRIENDS TABLE HERE WITH MySQL QUERY
 
 INSERT INTO Games (`title`,`rating`) VALUES ("Frogger 2","K+");
 INSERT INTO Games (`title`,`rating`) VALUES ("Syro, Year of the Dragon","K+");
