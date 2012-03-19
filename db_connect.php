523d326cbf63f7380862860d6f9fdfaaaba5116d
kproffitt <kproffit@mail.umw.edu>
2012-03-19 17:22:08 -0400

    Installer complete.  Removed db_connect.php from the repository, as each user of Luchalink will make their own unique db_connetc.php when the installer is run.

--- db_connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-$db = new mysqli('localhost', 'luchauser', 'kevinislame', 'Luchalink');
-if($db->connect_errno) {
-echo "Failed to connect to MySQL: (" . $db->connect_errno . ") " . $db->connect_error;
-}
