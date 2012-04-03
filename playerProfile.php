1d66b6e3c7b252ebc635ee1abbb595d209bcdfe0
Ross Kinsman <kinsman.ross@gmail.com>
2012-04-03 00:36:18 -0400

    madness is setting in, i fear for our site

--- playerProfile.php
+++ playerProfile.php
@@ -16,11 +16,8 @@
      <div class="container-fluid">
 	<?php include 'topbar.php' ?>
           <div class="row-fluid">
-               <div id="title-box">
-                    <h1><?php echo $name ?></h1>
-               </div>
                <div class="span2" id="sidebar">
-                    <h1>HERE BE THEIR PROFILE PIC</h1>
+                    <img src="luchamask.png"/>
                     <h2><?php echo $name ?></h2>
                     Info:
                     <ul>
@@ -36,6 +33,9 @@
                </div><!--sidebar-->
  
                <div class="span8" id="body">
+               <div id="title-box">
+                    <h1><?php echo $name ?></h1>
+               </div>
                     I'm  not entirely sure what goes here.
                     Probably all the games they play, most recent matches, short self-description?
                </div><!--body-->
